b62807c59e38a763df1e845f8246962804545db1
usupsuparma <usup.suparma7@gmail.com>
2026-06-27 17:48:17 +0700

    use ref

---
+++ src/states/authUser/action.js
@@ -0,0 +1,49 @@
+/**
+ * @TODO: Define all the actions (creator) for the authUser state
+ */
+
+import api from '../../utils/api';
+
+const ActionType = {
+  SET_AUTH_USER: 'SET_AUTH_USER',
+  UNSET_AUTH_USER: 'UNSET_AUTH_USER',
+};
+
+function setAuthUserActionCreator(authUser) {
+  return {
+    type: ActionType.SET_AUTH_USER,
+    payload: {
+      authUser,
+    },
+  };
+}
+
+function unsetAuthUserActionCreator() {
+  return {
+    type: ActionType.UNSET_AUTH_USER,
+  };
+}
+
+function asyncSetAuthUser({ id, password }) {
+  return async (dispatch) => {
+    const token = await api.login({ id, password });
+    api.putAccessToken(token);
+    const authUser = await api.getOwnProfile();
+    dispatch(setAuthUserActionCreator(authUser));
+  };
+}
+
+function asyncUnsetAuthUser() {
+  return async (dispatch) => {
+    dispatch(unsetAuthUserActionCreator());
+    api.putAccessToken('');
+  };
+}
+
+export {
+  ActionType,
+  setAuthUserActionCreator,
+  unsetAuthUserActionCreator,
+  asyncSetAuthUser,
+  asyncUnsetAuthUser,
+};
